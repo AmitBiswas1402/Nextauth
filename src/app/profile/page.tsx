f86313c9b8d8114be44426152d318edd50ca5589
Amit Biswas <amit.142biswas@gmail.com>
2024-03-31 22:42:57 +0530

    will do later

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React,{useState} from 'react'
+import axios from 'axios'
+import Link from 'next/link'
+import toast, { Toast } from 'react-hot-toast'
+import { useRouter } from 'next/router'
 
-function page() {
+export default function ProfilePage() {
+  const router = useRouter()
+  const [data, setData] = useState("nothing")
+
+  const getUserDetails = async () => {
+    const res = await axios.post('/api/users/me') 
+    console.log(res.data);
+    setData(res.data._id)    
+  }
+
+  const logout = async () => {
+    try {
+      await axios.get('/api/users/logout')
+      toast.success("logged out successfully")
+    } catch (error: any) {
+      console.log(error.message);
+      toast.error(error.message)                  
+    }
+  }
+
   return (
     <div>Profile page</div>
   )
 }
 
-export default page
